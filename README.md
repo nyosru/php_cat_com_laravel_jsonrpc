74444ed4ac3bcd97ef4554f2f6d6796d500b42ab
Sergey <1@php-cat.com>
2021-05-29 15:15:05 +0500

    0.6.0

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # php-cat-com-laravel-jsonrpc
  функции для работы с json-rpc в ларавели
+
+
+--- установка ---
+composer require php_cat_com/laravel_jsonrpc
+
+--- код использования в контроллере ---
+
+подключите RunController
+
+    use RunController; // ( любой другой, везде замените название )
+
+
+
+и в функции запускаем обработку ( приём запроса, отправка в метод класса и возвращаем результат )
+
+    public static function jsonRpcRequest(Request $request, RunController $controller)
+    {
+        try {
+            $result = JsonRpc::sendRequest( $request, $controller );
+            return JsonRpc::success($result, $request['id']);
+        } catch (\Exception $e) {
+            return JsonRpc::error($e->getMessage());
+        }
+    }
--- src/JsonRpc.php
+++ src/JsonRpc.php
@@ -7,20 +7,15 @@ namespace php_cat_com;
  */
 class JsonRpc
 {
-    public static function sendRequest()
-    {
 
-        $array = [ 111 ];
-
-        return $array;
-    }
-
-
+    // public static function sendRequest()
+    // {
+    //     $array = [111];
+    //     return $array;
+    // }
 
     const JSON_RPC_VERSION = '2.0';
 
-
-    
     public static function success($result, string $id = null)
     {
         return [
@@ -37,6 +32,22 @@ class JsonRpc
             'error'  => $error,
             'id'      => null,
         ];
+    }
+
+    /**
+     * отправляем запрос в нужный метод подключённого класса
+     */
+    public static function sendRequest($request, $controller)
+    {
+        if (
+            empty($request['method']) ||
+            empty($request['params']) ||
+            empty($request['id'])
+        ) {
+            throw new \Exception('Нехватает переменных');
+        }
+
+        return $controller->{$request['method']}(...[$request['params']]);
     }
 
 }
